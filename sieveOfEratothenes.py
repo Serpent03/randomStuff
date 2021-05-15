8c884db54b69ad04a532db36e731ac8f91e7a1e5
unknown <dumbpilot03@gmail.com>
2021-05-15 23:47:19 +0530

    SOE for prime numbers | 15.05.21

---
+++ sieveOfEratothenes.py
@@ -0,0 +1,32 @@
+import random
+
+def sieveMethod(num):
+    primeList = []
+    
+    a = [True] * num                          # Initialize the primality list
+    a[0] = a[1] = False
+
+    for (i, isprime) in enumerate(a):
+        if isprime:
+            primeList.append(i)
+            for n in range(i*i, num, i):     # Mark factors non-prime
+                a[n] = False
+
+    return primeList
+
+def writeKeys():
+    with open('keys.txt', 'w') as f:
+        for i in sieveMethod(2 ** 20):
+            if i > 2 ** 20 // 32:
+                f.write(f'{i}\n')
+
+def readKeys():
+    with open('keys.txt', 'r+') as f:
+        data = []
+
+        for nums in f.readlines():
+            data.append(int(nums.strip()))
+        
+    val = random.choice(data) * random.choice(data)
+
+    return val
