d40271ecb517a77797b7c64fa511e9d6012ef124
unknown <dumbpilot03@gmail.com>
2021-05-16 12:41:07 +0530

    Shifted to RSA(basic) and unicode | 16.05.21

--- encryption.py
+++ encryption.py
@@ -1,72 +1,41 @@
 # need to implement ord(char) into this lol
 
-from random import randint, choice
+import sieveOfEratothenes
 
-encryptionLookupValues = dict(
-    charList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
-    numList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
-    operatorList = ["'", " ", '"', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '[', ']', ':', ';', '{', '}', '/', '?', '<', '>', ',', '.', '`', '~', '|'],
-    charDecrypt = ['XdhR', 'SUbf', 'WHdb', 'DbRvE', 'WOFfc', 'DOHFF', 'cRFnc', 'XIHfv', 'xcUF'],
-    operDecrypt = ['iAFg', 'WD8v', 'asucE', 'xBRcR', 'iCivw', 'CNUWd', 'IWbfbc', 'CFCcWD', 'cIWFB'],
-    numDecrypt = ['XBRc', 'jTudD', 'URVF', 'cByrF', 'DIOfd', 'CFbnnf', 'xOFnF', 'pCHC', 'fqhog'],
-)
+# generate new keys every time
+sieveOfEratothenes.writeKeys()
+
 
 def encryptWord(word):
 
     encryptedWord = ''
-    
-    for chars in word:
-        if chars.lower() in encryptionLookupValues['charList']:     #pass upper and lower chars accordingly
-            encryptKey = choice(encryptionLookupValues['charDecrypt'])
-            if chars.islower():
-                rand = randint(1, 1024)
-                indexVal = encryptionLookupValues['charList'].index(chars)
-                encryptedWord += f'_{encryptKey}_{rand + indexVal}_{rand+2}_1{rand}'
-            elif chars.isupper():
-                rand = randint(1, 1024)
-                indexVal = encryptionLookupValues['charList'].index(chars.lower())
-                encryptedWord += f'_{encryptKey}_{rand + indexVal}_{rand+5}_2{rand}'
 
-        elif chars in encryptionLookupValues['numList']:    # number encryption
-            rand = randint(1, 1024)
-            indexVal = encryptionLookupValues['numList'].index(chars)
-            encryptKey = choice(encryptionLookupValues['numDecrypt'])
-            encryptedWord += f'_{encryptKey}_{rand + indexVal}_{rand+10}'
+    for char in word:
+        encryptionKey = round(sieveOfEratothenes.readKeys(), 0)
+        encryptionWord = f"{ord(char) * encryptionKey}_{encryptionKey}f_"
+        encryptedWord += encryptionWord
 
-        elif chars in encryptionLookupValues['operatorList']:
-            rand = randint(1, 1024)
-            indexVal = encryptionLookupValues['operatorList'].index(chars)
-            encryptKey = choice(encryptionLookupValues['operDecrypt'])
-            encryptedWord += f'_{encryptKey}_{rand + indexVal}_{rand*2}'
-        
-    print(f"\nEncrypted: {encryptedWord}\n")
+    print(f"\nEncrypted object: {encryptedWord}\n")
     return decryptWord(encryptedWord)
+    
 
 def decryptWord(encryptedQuery):
 
     decryptedWord = ''
     modulator = encryptedQuery.split('_')
 
-    for i in modulator:       # set up passed modulator
+    for i in modulator:
         if i == '':
             modulator.remove(i)
 
-    #return modulator
+    for key in range(len(modulator)):
+        try:
+            if modulator[key+1][-1] == 'f':     # modulator[key] == encrypted message. modulator[key+1] == passkey
+                decryptedWord += chr(int(modulator[key])//int(modulator[key+1][:-1]))
+        except:
+            pass
 
-    for char in range(len(modulator)):
-        if modulator[char] in encryptionLookupValues['charDecrypt']:  # check if passed letter is upper() or lower()
-            if modulator[char+3] == f"1{(int(modulator[char+2])-2)}":
-                decryptedWord += encryptionLookupValues['charList'][int(modulator[char+1]) - (int(modulator[char+2])-2)]
-            if modulator[char+3] == f"2{(int(modulator[char+2])-5)}":
-                decryptedWord += encryptionLookupValues['charList'][int(modulator[char+1]) - (int(modulator[char+2])-5)].upper()
-        
-        if modulator[char] in encryptionLookupValues['numDecrypt']:
-            decryptedWord += encryptionLookupValues['numList'][int(modulator[char+1]) - (int(modulator[char+2])-10)]
-        
-        if modulator[char] in encryptionLookupValues['operDecrypt']:
-            decryptedWord += encryptionLookupValues['operatorList'][int(modulator[char+1]) - (int(modulator[char+2])//2)]
-    
-    return f"Decrypted: {decryptedWord}"
+    return f"Decrypted object: {decryptedWord}"
 
 if __name__ == "__main__":
     print(encryptWord(str(input("Input: "))))
